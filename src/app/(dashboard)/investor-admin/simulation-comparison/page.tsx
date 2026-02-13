a6a569cc4a8c9023346bd79a494159fc305a26ec
johir-dev <pappurohan1100@gmail.com>
2026-02-14 04:46:24 +0600

    added

--- src/app/(dashboard)/investor-admin/simulation-comparison/page.tsx
+++ src/app/(dashboard)/investor-admin/simulation-comparison/page.tsx
@@ -4,9 +4,9 @@ import React from 'react'
 const page = () => {
   return (
     <div>
-         <SimulationComparison onBack={function (): void {
+         {/* <SimulationComparison onBack={function (): void {
               throw new Error('Function not implemented.')
-          } } />
+          } } /> */}
     </div>
   )
 }
